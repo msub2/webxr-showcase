ae55c7fd3849543c11894ff4bf7796c63bbf597a
msub2 <msub2official@gmail.com>
2021-02-21 16:23:53 -0500

    Replaced header with custom element, updated pages

---
+++ docs/components.js
@@ -0,0 +1,62 @@
+class M2Header extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'header');
+
+        // Logo
+        const logo = document.createElement('h1');
+        logo.setAttribute('id', 'title');
+        logo.innerText = 'm₂';
+
+        // Home link
+        const home = document.createElement('a');
+        home.setAttribute('href', '/');
+        home.innerText = 'Home';
+
+        // Silk Brush link
+        const silkbrush = document.createElement('a');
+        silkbrush.setAttribute('href', '/silk-brush/');
+        silkbrush.innerText = 'Silk Brush';
+
+        // Silk Brush files link
+        const files = document.createElement('p');
+        files.innerHTML = '(<a href="/silk-brush/files/">Files</a>)';
+
+        // WebXR Showcase
+        const showcase = document.createElement('a');
+        showcase.setAttribute('href', "/webxr-showcase");
+        showcase.innerText = 'WebXR Showcase';
+
+        // Styling
+        const style = document.createElement('style');
+        style.textContent = `
+            h1 {
+                font-size: 4em;
+            }
+
+            .header > a {
+                margin: 1em;
+            }
+
+            p {
+                display: inline;
+                margin-left: -.5em;
+            }
+        `;
+
+        shadowRoot.append(style);
+        wrapper.appendChild(logo);
+        wrapper.appendChild(home);
+        wrapper.appendChild(silkbrush);
+        wrapper.appendChild(files);
+        wrapper.appendChild(showcase);
+        wrapper.appendChild(document.createElement('hr'));
+
+        shadowRoot.append(wrapper);
+    }
+}
+customElements.define('m2-header', M2Header);
